3f020523ce242d208e9035a9dc7dab6e1f0132c8
hardikcloud <techhardik321@gmail.com>
2026-02-24 18:24:57 +0530

    chatbot working

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from ollama_service import get_ai_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.get_json()
+    user_message = data.get("message")
+
+    if not user_message:
+        return jsonify({"reply": "No message received."})
+
+    reply = get_ai_response(user_message)
+
+    return jsonify({"reply": reply})
 
 if __name__ == "__main__":
     app.run(debug=True)
